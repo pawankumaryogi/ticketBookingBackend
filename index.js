e9137c9b902511ba615dc3c25f62b0c65ac4f037
pawanyogi <yogipawanofficial01@gmail.com>
2024-01-05 22:31:16 +0530

    cors url added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors()); 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://ticket-booking-nu.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
