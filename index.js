c0e09675ccefcf394226c7079fb107d52ccf496c
Pavan Jogi <pavan1502873@mastek.com>
2024-01-29 14:39:04 +0530

    cors header added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
  
 app.use(express.json()); 
 app.use(cors({
-    origin: ['https://ticket-booking-nu.vercel.app', 'http://localhost:3000'],
+    origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }));
 
