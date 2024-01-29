322cc83bca4c51961c1fcab0bcb49adc8d301dac
Pavan Jogi <pavan1502873@mastek.com>
2024-01-29 14:43:51 +0530

    cors header added

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const app = express();
  
 app.use(express.json()); 
 app.use(cors({
-    origin: ['*'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    origin: '*'
   }));
 
 app.get('/', (req, res) => { 
